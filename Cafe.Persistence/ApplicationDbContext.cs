556af9ac3b91a36a981f3d0175be5712063e5b06
Manuel Hernandez <mahmaruro@outlook.com>
2023-05-12 07:07:53 -0600

    crear proyecto

---
+++ Cafe.Persistence/ApplicationDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Cafe.Model;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cafe.Persistence
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ApplicationUser>();
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ Cafe.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Cafe.Web.Startup))]
+namespace Cafe.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
